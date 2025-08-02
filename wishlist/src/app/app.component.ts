0d0c538d5c65cefb3273633bf405152417987004
willsf2021 <willsf2021@gmail.com>
2025-08-02 11:32:16 -0300

    feat: taking decisions and using loop with directives and nd-templates

--- wishlist/src/app/app.component.ts
+++ wishlist/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { WishItem } from 'src/shared/models/wishItem';
 
-@Component({
+@Component({ // @ = Decorator
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wishlist';
+  items: WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ];
+  title = 'my wishlist';
 }
